368a7c26ac1c888c668ba3340b2b6d30d6c1edec
Talha Masood <scortalz@hotmail.com>
2017-09-11 10:40:40 +0500

    checking

--- application/views/reception/manage_invoice.php
+++ application/views/reception/manage_invoice.php
@@ -349,6 +349,7 @@
                                        <option value="B+">B+</option>
                                        <option value="B-">B-</option>
                                        <option value="AB+">AB+</option>
+                                       <option value="AB+">AB+1</option>
                                        <option value="AB-">AB-</option>
                                        <option value="O+">O+</option>
                                        <option value="O-">O-</option>
